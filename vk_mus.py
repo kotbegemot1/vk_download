244d7812ddc070eded7ce33a6e4d8106cdb28ca2
kotbegemot <kk1slorod@gmail.com>
2021-03-09 19:48:53 +0300

    init commit

--- vk_mus.py
+++ vk_mus.py
@@ -7,9 +7,9 @@ import os
 REQUEST_STATUS_CODE = 200
 name_dir = 'music_vk'
 path = '/home/kotbegemot/Документы/' + name_dir
-login = 'kk1slorod@gmail.com'
-password = 'proimbagosupers1ibhjvfcjdj1'
-my_id = 'id20501806'
+login = ''
+password = ''
+my_id = ''
 
 if not os.path.exists(path):
 	os.makedirs(path)
