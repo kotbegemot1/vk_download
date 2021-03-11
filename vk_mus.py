e0d327aa451ae6140767e747edf685f0a5aa0cbe
qwe <qwe@qwe>
2021-03-11 19:52:46 +0300

    commit from pk

--- vk_mus.py
+++ vk_mus.py
@@ -13,3 +13,9 @@ my_id = ''
 
 if not os.path.exists(path):
 	os.makedirs(path)
+
+vk_session = vk_api.VkApi(login=login, password=password)
+vk_session.auth()
+vk = vk_session.get_api()  # Теперь можно обращаться к методам API как к обычным 
+                                        # классам
+vk_audio = audio.VkAudio(vk_session)
