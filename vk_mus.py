78514cdd949ac92322823bb2023ec8acb8d50cd0
qwe <qwe@qwe>
2021-03-21 18:37:40 +0300

    add search and download

--- vk_mus.py
+++ vk_mus.py
@@ -7,7 +7,7 @@ import info
 
 REQUEST_STATUS_CODE = 200
 name_dir = 'music_vk'
-path = '/home/kotbegemot/Документы/' + name_dir
+path = info.path_mus + name_dir
 login = info.login
 password = info.password
 my_id = info.my_id
@@ -22,3 +22,12 @@ vk_session.auth()
 vk = vk_session.get_api()  # Теперь можно обращаться к методам API как к обычным 
                                         # классам
 vk_audio = audio.VkAudio(vk_session)
+
+qwe = vk_audio.search("nice", 10, 0)
+
+r = next(qwe)
+
+req = requests.get(r["url"])
+
+with open(r["artist"] + '_' + r["title"] + '.mp3', 'wb') as output_file:
+	output_file.write(req.content)
