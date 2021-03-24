3737e296ad36efb61a0388b5f3e82b1f26d6fabe
qwe <qwe@qwe>
2021-03-24 21:47:48 +0300

    add input

--- vk_mus.py
+++ vk_mus.py
@@ -12,7 +12,7 @@ login = info.login
 password = info.password
 my_id = info.my_id
 
-print(login, password, my_id, sep='')
+# print(login, password, my_id, sep='')
 
 if not os.path.exists(path):
 	os.makedirs(path)
@@ -25,11 +25,46 @@ vk_audio = audio.VkAudio(vk_session)
 
 os.chdir(path)
 
-qwe = vk_audio.search("nice", 10, 0)
+question = input("Введите название песни: ")
 
-r = next(qwe)
+tracklist = []
+for track in enumerate(vk_audio.search(question, 3, 0), 1):
+	tracklist.append(track)
+	print("{}. {} - {}".format(track[0], track[1]['artist'], track[1]['title']))
 
-req = requests.get(r["url"])
+number_of_track = input("Введите порядковый номер песни которую хотите скачать: ")
 
-with open(r["artist"] + '_' + r["title"] + '.mp3', 'wb') as output_file:
-	output_file.write(req.content)
+for artist in tracklist:
+	if artist[0] == int(number_of_track):
+		req = requests.get(artist[1]["url"])
+		with open(artist[1]["artist"] + '_' + artist[1]["title"] + '.mp3', 'wb') as output_file:
+			output_file.write(req.content)
+			print('downloaded')
+		break
+	else:
+		print('not downloaded')
+		continue
+
+if __name__ == '__main__':
+	pass
+
+
+# qwe = vk_audio.search(str(question), 10, 0)
+
+# r = next(qwe)
+
+# req = requests.get(r["url"])
+
+# with open(r["artist"] + '_' + r["title"] + '.mp3', 'wb') as output_file:
+# 	output_file.write(req.content)
+
+# tracklist = []
+# for track in enumerate(vk_audio.search('love', 2, 0), 1):
+# 	tracklist.append(track)
+# 	print("{}. {} - {}".format(track[0], track[1]['artist'], track[1]['title']))
+
+# for artist in tracklist:
+# 	if artist[0] == 1:
+# 		req = requests.get(artist[1]["url"])
+# 		with open(artist[1]["artist"] + '_' + artist[1]["title"] + '.mp3', 'wb') as output_file:
+# 			output_file.write(req.content)
